209fd886196a434f58c9b77797acd46aefad261c
SeanSFitz <sean.s.fitz@gmail.com>
2017-06-20 21:19:40 +0000

    added foreign key to milestones table

---
+++ migrations/20170620210854_edit_milestones.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', (table) => {
+      table.integer('famous_id').unsigned();
+      table.foreign('famous_id').references('famous_people.id');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', (table) => {
+      table.dropForeign('famous_id')
+    })
+  ])
+};
